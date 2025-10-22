12b3972fa9cdfe34604a69833fa4e3e5fb47f010
Ali asghar sedighian job <as.sedighian@gmail.com>
2025-10-22 15:26:45 +0330

    add header for product categories

--- app/dashboard/products/categories/page.tsx
+++ app/dashboard/products/categories/page.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ProductCategoriesTable from '../components/ProductCategoryTable';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { PlusCircle } from 'lucide-react';
 
 interface Props {
   searchParams: Promise<{
@@ -29,7 +32,40 @@ async function ProductCategoriesPage({ searchParams }: Props) {
 
   const categories = await getAllProductCategories(page, per_page);
   return (
-    <div>
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center">
+        <div className="flex items-center">
+          <Button variant="link">
+            <Link href="/dashboard/products?page=1">Products</Link>
+          </Button>
+          <Button variant="link">
+            <Link
+              className="underline"
+              href="/dashboard/products/categories?page=1"
+            >
+              Products Categories
+            </Link>
+          </Button>
+        </div>
+        <div className="ml-auto flex items-center gap-2">
+          <Button variant="secondary" asChild size="sm" className="h-8 gap-1">
+            <Link href="/dashboard/products/create-category">
+              <PlusCircle className="h-3.5 w-3.5" />
+              <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                Add Category Product
+              </span>
+            </Link>
+          </Button>
+          <Button asChild size="sm" className="h-8 gap-1">
+            <Link href="/dashboard/products/create">
+              <PlusCircle className="h-3.5 w-3.5" />
+              <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                Add Product
+              </span>
+            </Link>
+          </Button>
+        </div>
+      </div>{' '}
       <ProductCategoriesTable
         categories={categories.productCategories}
         totalProductCategories={categories.totalCount}
